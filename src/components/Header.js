8caacba5eda75393efa12f92f26b649e3ac291ea
Marc John Corral <103993554+mjc1012@users.noreply.github.com>
2022-08-06 10:25:50 +0800

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types' 
+import Button from "./Button"
+
+const Header = ({title, onAdd, showAddTask}) => {
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color={showAddTask? "red" : "green"} text={showAddTask? "Close" : "Add"} onClick={onAdd}></Button>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+// CSS in JS
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black',
+// }
+export default Header
